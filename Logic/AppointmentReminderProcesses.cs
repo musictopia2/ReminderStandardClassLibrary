86a91d62d1be76cb3b7fb2e998b8c73585c6a172
Andy <musictopia2@hotmail.com>
2020-01-27 10:15:43 -0700

    Cleanup

--- Logic/AppointmentReminderProcesses.cs
+++ Logic/AppointmentReminderProcesses.cs
@@ -1,8 +1,5 @@
 ﻿using ReminderStandardClassLibrary.DataAccess;
 using ReminderStandardClassLibrary.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ReminderStandardClassLibrary.Logic
