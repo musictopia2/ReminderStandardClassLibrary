7f7177cee5e3d26dae1673b339d15486b2bbc5fa
Andy <musictopia2@hotmail.com>
2020-01-28 10:51:22 -0700

    Removed folder for view models since that needs to be further down now.

--- GeneralViewModels/Enums.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ReminderStandardClassLibrary.GeneralViewModels
-{
-	
-	//waitingforuser means waiting for multiple step.
-	//the waitingforreminder is waiting for reminder or waiting for popups to close
-	//none means waiting for user action.
-}
