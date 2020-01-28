7e69f28e42cedbf60056e32fc7e34cb02fa01abe
Andy <musictopia2@hotmail.com>
2020-01-27 20:01:21 -0700

    Added tostring for default text for temporary reoccuring activities.

--- Models/TemporaryReoccuringReminderModel.cs
+++ Models/TemporaryReoccuringReminderModel.cs
@@ -9,16 +9,23 @@ namespace ReminderStandardClassLibrary.Models
         public EnumTimeFormat TimeMode { get; set; } //will be hours or days but this allowed extra flexibility.
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
-        //i think its okay to delete if its past time. and already fulfilled all.
-        //for now, this may be okay.
-        //can test the mock version.
 
         public int HowMany { get; set; } //this is how many days or how many hours.
 
         public string Message { get; set; } = ""; //this is what needs to be done.
 
-
-
+        public override string ToString()
+        {
+            if (TimeMode == EnumTimeFormat.Days)
+            {
+                return $"{Message} starting {StartDate} ending {EndDate} every {HowMany} days";
+            }
+            if (TimeMode == EnumTimeFormat.Hours)
+            {
+                return $"{Message} starting {StartDate} ending {EndDate} every {HowMany} hours";
+            }
+            return base.ToString(); //can later see what the message should be in other cases.
+        }
 
 
     }
